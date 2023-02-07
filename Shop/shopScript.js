9a2ff344d5432f5be042b6f707945ffb6a01b47d
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-02-07 18:47:41 +0800

    Update and rename app.js to shopScript.js

--- Shop/shopScript.js
+++ Shop/shopScript.js
@@ -4,7 +4,7 @@ const cartTable = document.querySelector('#cart tbody');
 const totalDisplay = document.querySelector('#total');
 
 let total = 0;
-
+totalDisplay.textContent = `$${total.toFixed(2)}`;
 // Loop through all "Add to Cart" buttons
 addToCartButtons.forEach((button) => {
     button.addEventListener('click', () => {
@@ -15,27 +15,27 @@ addToCartButtons.forEach((button) => {
 
         // Create a new row in the cart table
         const row = document.createElement('tr');
-        row.innerHTML = `
+         row.innerHTML = `
         <td>${name}</td>
         <td>1</td>
         <td>${price}</td>
-        <td><button class="remove">Remove</button></td>
+        <td><img class="remove-img" src="/icons/remove.png" alt="Remove Item"></td>
         `;
         cartTable.appendChild(row);
-
-        // Update the total amount
-        total += parseFloat(price.slice(1));
+        const priceNumber = parseFloat(price.replace('$', ''));
+        // console.log(price.slice(2))
+        total += parseFloat(priceNumber);
         totalDisplay.textContent = `$${total.toFixed(2)}`;
 
         // Add a click event listener to the "Remove" button
-        const removeButton = row.querySelector('.remove');
-        removeButton.addEventListener('click', () => {
-        // Remove the row from the cart table
-        cartTable.removeChild(row);
+        const removeImg = row.querySelector('.remove-img');
+        removeImg.addEventListener('click', () => {
+            // Remove the row from the cart table
+            cartTable.removeChild(row);
 
-        // Update the total amount
-        total -= parseFloat(price.slice(1));
-        totalDisplay.textContent = `$${total.toFixed(2)}`;
+            // Update the total amount
+            total -= parseFloat(priceNumber);
+            totalDisplay.textContent = `$${total.toFixed(2)}`;
         });
     });
 });
