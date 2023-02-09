e6114fad5cdbd03b6da6fc742f18955d6c20f70b
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-02-09 17:34:46 +0800

    Update shopScript.js

--- Shop/shopScript.js
+++ Shop/shopScript.js
@@ -1,40 +1,71 @@
-// Get the "Add to Cart" buttons and the cart table
 const addToCartButtons = document.querySelectorAll('.add-to-cart');
 const cartTable = document.querySelector('#cart tbody');
 const totalDisplay = document.querySelector('#total');
+const checkoutBtn = document.getElementById("checkout");
 
+
+// Constants
+let song = new Audio("songs/song1.mp3");
 let total = 0;
 totalDisplay.textContent = `$${total.toFixed(2)}`;
-// Loop through all "Add to Cart" buttons
+
+
+// Loading the shop... 
+window.addEventListener('load', function() {
+    song.play();
+});
+
+
+// User Clicking the Checkout Button
+checkoutBtn.addEventListener("click", function () {
+    localStorage.setItem("songTime", song.currentTime);
+    localStorage.setItem("orderTotal", total);
+    window.location.href = "checkoutIndex.html";
+});
+
+// Handling User Adding to Cart and Rmoving Items
 addToCartButtons.forEach((button) => {
     button.addEventListener('click', () => {
-        // Get the product information
+        // Product information
         const product = button.parentElement;
         const name = product.querySelector('.name').textContent;
         const price = product.querySelector('.price').textContent;
 
-        // Create a new row in the cart table
+        // Get the current items from localStorage
+        let items = JSON.parse(localStorage.getItem("items")) || [];
+
+        // Add the new item to the list of items
+        items.push({ name, price });
+
+        // Store the items in localStorage
+        localStorage.setItem("items", JSON.stringify(items));
+
+        // Inserting Product into Cart
         const row = document.createElement('tr');
-         row.innerHTML = `
-        <td>${name}</td>
-        <td>1</td>
-        <td>${price}</td>
-        <td><img class="remove-img" src="/icons/remove.png" alt="Remove Item"></td>
+        row.innerHTML = 
+        `
+            <td>${name}</td>
+            <td>1</td>
+            <td>${price}</td>
+            <td><img class="remove-img" src="/icons/remove.png" alt="Remove Item"></td>
         `;
         cartTable.appendChild(row);
         const priceNumber = parseFloat(price.replace('$', ''));
-        // console.log(price.slice(2))
         total += parseFloat(priceNumber);
         totalDisplay.textContent = `$${total.toFixed(2)}`;
-
-        // Add a click event listener to the "Remove" button
         const removeImg = row.querySelector('.remove-img');
+
+        // Removing Product from Cart
         removeImg.addEventListener('click', () => {
-            // Remove the row from the cart table
             cartTable.removeChild(row);
 
-            // Update the total amount
+            // Remove the item from the items in localStorage
+            items = items.filter((item) => item.name !== name);
+            localStorage.setItem("items", JSON.stringify(items));
+
+            // Updating amount
             total -= parseFloat(priceNumber);
+            if(total < 0) total *= -1; 
             totalDisplay.textContent = `$${total.toFixed(2)}`;
         });
     });
