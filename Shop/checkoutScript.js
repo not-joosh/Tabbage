6795dabf7cf06fd7c6d6c06fbafae01143bdcade
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-02-09 17:33:46 +0800

    Update checkoutScript.js

--- Shop/checkoutScript.js
+++ Shop/checkoutScript.js
@@ -1,6 +1,16 @@
 var modal = document.getElementById("alert-modal");
 var btn = document.getElementById("place-order-btn");
 var span = document.getElementsByClassName("close")[0];
+const orderTotalDisplay = document.getElementById("order-total-display");
+let song = new Audio("songs/song1.mp3");
+
+// Loadingg the checkout page...
+window.addEventListener('load', function() {
+    song.currentTime = this.localStorage.getItem("songTime");
+    song.play();
+    const orderTotal = localStorage.getItem("orderTotal");
+    orderTotalDisplay.textContent = `$${parseFloat(orderTotal).toFixed(2)}`;
+});
 
 btn.onclick = function() {
     if (!validateForm()) {
@@ -18,6 +28,8 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
+
+// Checking if Form is ready to be submitted
 function validateForm() {
     var inputs = document.getElementsByTagName("input");
     for (var i = 0; i < inputs.length; i++) {
@@ -27,3 +39,10 @@ function validateForm() {
     }
     return true;
 }
+
+// Handling user to go back to Shop
+const backButton = document.getElementById("back-button");
+backButton.addEventListener("click", function () {
+    localStorage.setItem("songTime", song.currentTime);
+    window.location.href = "shopIndex.html";
+});
