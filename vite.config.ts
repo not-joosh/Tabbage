f194b12981f84227d56e8729a71086bf1b21151c
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-05-01 00:33:07 +0800

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000, // increase the limit to 1000 kBs
+  },
+})
+
